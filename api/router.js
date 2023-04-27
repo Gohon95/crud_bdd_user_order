76fb2a713e9c76de59e141c381bfec83f11f3c74
Axel <axel@40-60studio.com>
2023-04-27 16:20:49 +0200

    Add commentaire

--- api/router.js
+++ api/router.js
@@ -1,10 +1,13 @@
 export default (controlers, app) => {
+  // Route Orders
   app.get("/status", controlers.statusCtrl.getStatus);
   app.get("/orders", controlers.orderCtrl.listOrders);
   app.get("/orders/:id", controlers.orderCtrl.getOrder);
   app.post("/orders", controlers.orderCtrl.createOrder);
   app.put("/orders/:id", controlers.orderCtrl.updateOrder);
   app.delete("/orders/:id", controlers.orderCtrl.deleteOrder);
+
+  // Route users
   app.get("/users", controlers.userCtrl.listUsers);
   app.get("/users/:id", controlers.userCtrl.getUser);
   app.post("/users", controlers.userCtrl.createUser);
